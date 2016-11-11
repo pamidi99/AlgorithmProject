f2925f569ee5ca9a621187eccb6ec4b34614e86b
bhanu <prakashbp2020@gmail.com>
2016-11-10 23:06:25 -0500

    second commit

--- AlgorithmProjects/src/InsertionSort.java
+++ AlgorithmProjects/src/InsertionSort.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
  * Sorts the data and stores the result into answer.txt.
  * Delimiter should be ","
  */
+//Second Commit
 public class InsertionSort {
 
 	public InsertionSort() {
--- AlgorithmProjects/src/RandomInputgenerator.java
+++ AlgorithmProjects/src/RandomInputgenerator.java
@@ -4,7 +4,7 @@
 import java.io.PrintWriter;
 
 
-
+//update
 public class RandomInputgenerator {
 	public static void main(String[] args) throws Exception 
 	{
